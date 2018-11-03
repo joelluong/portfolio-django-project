814265f659c4fe9f0536b6e2e93ebde1bd16df52
duongluong <101051766@student.swin.edu.au>
2018-11-03 15:51:43 +1100

    Blog page and Blog Details

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+# Create A Blog model here.
+# title
+# pub_date
+# body
+# image
+class Blog (models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
+
+    def summary(self):
+        return self.body[:100] + ' ...'
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
+
+# Add the Blog app to the settings
+# Create a migration
+# Migrate
+# Add to the admin
